90da7e523ebf2ee803289d8ae09dd7f4c5b9fe99
Italo Gabriel <italo.gabrielcs@hotmail.com>
2020-06-13 21:11:59 -0300

    first commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,71 @@
+// comment
+// document.write("Hello World")
+
+// // variaves, tipos de dados
+// var myvar //undefined
+// myvar = "Hello World!"
+// const myconst = "Oi Mundo"
+
+// document.write(myvar + myconst)
+
+// // string
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso é uma string'
+// const s3 = `Isso é uma string`
+
+// // calculadora
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// // boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// // objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// // Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidade - Atalhos - Reuso de código
+
+// // Registrar função
+// function sayMyName(name) {
+//   document.write(name)  
+// }
+
+// // Executar
+// sayMyName("Djavan")
+// sayMyName("Victor")
+// sayMyName("Hugo")
+
+// // Condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado")
+   
+//    } else {
+//     document.write("Aprovado")
+// }
+
+// // Loop ou repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write("<p>${i}</p>")
+  
+// }
---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const server = express()
+
+// Configurar pasta publica
+server.use(express.static("public"))
+
+// Utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// Configurar caminhos da minha aplicação
+// Página inicial
+// req = Requisição
+// res = Resposta
+server.get("/", (req, res) => {
+  return  res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+   return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
+// Ligar o servidor
+server.listen(3000)
